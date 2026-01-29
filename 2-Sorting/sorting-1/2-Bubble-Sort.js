d65705b383b767b3374d7989648706795f55d5cc
AyushNegixd <ayushnegi200051@gmail.com>
2026-01-29 10:56:48 +0530

    Solved with optimized logic

---
+++ 2-Sorting/sorting-1/2-Bubble-Sort.js
@@ -0,0 +1,20 @@
+// Given an array of integers called nums,sort the array in non-decreasing order using the bubble sort algorithm and return the sorted array.
+
+// A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
+
+
+class Solution {
+    bubbleSort(nums) {
+        let n = nums.length;
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n-1-i; j++){
+                if (nums[j] > nums[j+1]) {
+                    let temp = nums[j];
+                    nums[j] = nums [j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+};
