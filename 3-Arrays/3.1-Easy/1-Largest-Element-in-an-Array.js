2d5fd06a8faccd165ed7b6b71071d65ca4f5febf
AyushNegixd <ayushnegi200051@gmail.com>
2026-02-06 10:56:56 +0530

    Brute + Optimal approach

--- 3-Arrays/3.1-Easy/1-Largest-Element-in-an-Array.js
+++ 3-Arrays/3.1-Easy/1-Largest-Element-in-an-Array.js
@@ -1,5 +1,17 @@
 // Given an array of integers nums, return the value of the largest element in the array
 
+//brute force approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1) or O(n) depending on the implementation
+
+function largestElement(nums) {
+    nums.sort((a, b) => b - a);
+    return nums[nums.length - 1];
+}
+
+//optimal approach
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 function largestElement(nums) {
     if (nums.length === 0) {
         throw new Error("Array is empty");
