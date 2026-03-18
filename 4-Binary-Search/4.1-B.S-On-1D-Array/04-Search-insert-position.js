f4acb3c493dc80021ae42dc5a819c22f2046ec9b
AyushNegixd <ayushnegi200051@gmail.com>
2026-03-18 11:49:26 +0530

    Brute + Optimal Approach

---
+++ 4-Binary-Search/4.1-B.S-On-1D-Array/04-Search-insert-position.js
@@ -0,0 +1,34 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+
+// brute force approach
+// time complexity: O(n)
+// space complexity: O(1)
+var searchInsert = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
+    }
+    return nums.length;
+};
+
+// optimal approach (binary search)
+// time complexity: O(log n)
+// space complexity: O(1)
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
